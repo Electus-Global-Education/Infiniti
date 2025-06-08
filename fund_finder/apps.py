ba3d625334207980ed645df4997b5e22e250571b
Rana Faraz <ranafarazahmed@gmail.com>
2025-06-08 11:10:52 +0000

    Add ready method to FundFinderConfig for signal imports on startup

--- fund_finder/apps.py
+++ fund_finder/apps.py
@@ -1,6 +1,13 @@
+# fund_finder/apps.py
 from django.apps import AppConfig
-
 
 class FundFinderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'fund_finder'
+
+    def ready(self):
+        """
+        This method is called when Django starts. We import our signals here
+        to ensure they are connected.
+        """
+        import fund_finder.signals
